767664af47972f30f2e7702cac0fa9791c8d8dfa
Camilo Gonçalves <camilolgon@gmail.com>
2019-12-10 18:56:06 -0300

    Level 3 challenge complete.

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,42 @@
 from flask import Flask, request, jsonify
 
 
-def _compute_cart_total(items, articles):
+def _compute_discount(article_price, discount):
+    '''Helper function that computes an item's discount amount.
+
+    :param article_price: The product's original price.
+    :param discount: The product's discount information.
+        There are two kinds of discounts:
+        - a direct cut to the article's price, e.g. get 50€ off your 300€ caviar tin and only pay 250€
+        - a percentage discount, e.g. get 20% off your 5€ creme fraiche and only pay 4€
+    '''
+    # Direct cut to the article's price
+    if discount['type'] == 'amount':
+        return article_price - discount['value']
+    # Percentage discount
+    else:
+        return int(article_price * (1 - discount['value'] / 100))
+
+
+def _compute_cart_total(items, articles, discounts=None):
     '''Helper function that computes the sum of the prices of articles in one cart.
 
     :param items: The list of items of the cart.
-    :param articles: Dictionary conaining the data of all items
+    :param articles: Dictionary containing the data of all items.
+    :param discounts: Dictionary containing the discounts data. If not supplied, no discounts are applied.
     :returns: The total sum of the cart items.
     '''
     total = 0
     for item in items:
-        article = articles[item['article_id']]
-        total += article['price'] * item['quantity']
+        article_id = item['article_id']
+        article = articles[article_id]
+        article_price = article['price']
+
+        if discounts is not None:
+            discount = discounts.get(article_id, {'type': 'amount', 'value': 0})
+            article_price = _compute_discount(article_price, discount)
+
+        total += article_price * item['quantity']
     return total
 
 
@@ -19,7 +44,8 @@ def _get_delivery_fee(cart_total, delivery_fees):
     '''Helper function that retrieves the delivery fee cost of a cart given its total cost.
 
     :param cart_total: The total cost of a cart items.
-    :param delivery_fee: A list of tuples containing the transaction volume min and max of the delivery fees and their cost values.
+    :param delivery_fee: A list of tuples containing the transaction volume min and max of the delivery fees and
+    their cost values.
     :returns:  The delivery fee cost of the cart.
     '''
     return next(
@@ -51,9 +77,22 @@ def create_app(config=None):
             for item in articles
         }
 
+        # Retrieves the discounts, if they were supplied.
+        discounts = data.get('discounts', None)
+
+        if discounts is not None:
+            # Turns the list of discounts into a dictionary. This will facilitate the retrieving of the items data.
+            discounts = {
+                item['article_id']: {'type': item['type'], 'value': item['value']}
+                for item in discounts
+            }
+
         # Computes the sum of the prices of articles in each cart.
         carts_totals = [
-            {'id': cart['id'], 'total': _compute_cart_total(cart['items'], articles)}
+            {
+                'id': cart['id'],
+                'total': _compute_cart_total(cart['items'], articles, discounts),
+            }
             for cart in carts
         ]
 
