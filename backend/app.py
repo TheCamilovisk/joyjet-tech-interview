d2e34daf8dcaf7006689dd8943a9823a4c19ac6e
Camilo Gonçalves <camilolgon@gmail.com>
2019-12-10 17:07:42 -0300

    Level 2 challenge complete.

--- backend/app.py
+++ backend/app.py
@@ -15,6 +15,20 @@ def _compute_cart_total(items, articles):
     return total
 
 
+def _get_delivery_fee(cart_total, delivery_fees):
+    '''Helper function that retrieves the delivery fee cost of a cart given its total cost.
+
+    :param cart_total: The total cost of a cart items.
+    :param delivery_fee: A list of tuples containing the transaction volume min and max of the delivery fees and their cost values.
+    :returns:  The delivery fee cost of the cart.
+    '''
+    return next(
+        cost
+        for min_price, max_price, cost in delivery_fees
+        if cart_total >= min_price and (max_price is None or cart_total < max_price)
+    )
+
+
 def create_app(config=None):
     app = Flask(__name__)
     if config == 'test':
@@ -22,10 +36,10 @@ def create_app(config=None):
 
     @app.route('/checkout', methods=['POST'])
     def checkout():
-        # Retrieve the request data.
+        # Retrieves the request data.
         data = request.get_json()
 
-        # Retrieve the articles and carts data. Return an error if none of them are found.
+        # Retrieves the articles and carts data. Return an error if none of them are found.
         articles = data.get('articles', None)
         carts = data.get('carts', None)
         if articles is None or carts is None:
@@ -42,6 +56,31 @@ def create_app(config=None):
             {'id': cart['id'], 'total': _compute_cart_total(cart['items'], articles)}
             for cart in carts
         ]
+
+        # Retrieves the delivery fees, if they were supplied.
+        delivery_fees = data.get('delivery_fees', None)
+
+        # Applies the delivery fees, if they were supplied.
+        if delivery_fees is not None:
+            # Turns the list of delivery fees dictionaries into a list of tuples, for easy manipulation.
+            delivery_fees = [
+                (
+                    fee['eligible_transaction_volume']['min_price'],
+                    fee['eligible_transaction_volume']['max_price'],
+                    fee['price'],
+                )
+                for fee in delivery_fees
+            ]
+
+            # Applies the delivery fee of each cart.
+            carts_totals = [
+                {
+                    **cart,
+                    'total': cart['total']
+                    + _get_delivery_fee(cart['total'], delivery_fees),
+                }
+                for cart in carts_totals
+            ]
 
         return jsonify(carts=carts_totals), 200
 
